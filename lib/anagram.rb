2859eff30a2c6914c43f1fdedbd66d0484afa8c3
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 19:37:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,12 @@ class Anagram
   
   attr_accessor :word
   
+  
   def initialize(word)
     @word = word
   end
   
+    
   def match(array_of_words)
     
     array_of_words.select do |element|
