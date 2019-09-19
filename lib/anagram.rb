d700f4c0ca0256c1229ca17c074f56984a53aa33
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 19:39:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   
   def match(array_of_words)
     
+    # Out of the array, select the anagram 
     # 
     array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
