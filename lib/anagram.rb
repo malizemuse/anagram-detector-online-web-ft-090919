a85fcf1cc98338d8c7cf601ac3f99d5c7932690c
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 19:42:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array_of_words)
+    
+    # Out of the array, select the anagram 
+    # Split word and each element into indiv letters 
+    # If resulting sorted split word and element are equal, then return matching element
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+    
+  end
+  
+end
+
+
